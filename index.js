9a5e9bba8846b05c0b0780f876788b1c1965bccd
Tom Scanlan <veerscanlan@gmail.com>
2018-10-09 23:06:30 -0500

    three

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/User');
+require("./services/passport");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req,res)=>{
-    res.send({bye:"buddy"})
-});
+require('./routes/authRoutes')(app);
 
+console.log("Server Online...")
 const PORT = process.env.PORT || 3000;
 app.listen(PORT);
